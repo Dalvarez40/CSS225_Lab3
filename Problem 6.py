c4ded537d49f1dad4c66e505b719f86aacda6070
Dalvarez40 <146905363+Dalvarez40@users.noreply.github.com>
2023-10-16 15:48:20 -0500

    Add files via upload

---
+++ Problem 6.py
@@ -0,0 +1,15 @@
+#Daniel Alvarez
+#10/16/23
+
+#Problem 6 Lab 3
+
+#This program converts degrees Fahrenheit into degrees Celsius
+
+#Asking User to input the temperature in degrees Fahrenheit
+Fahrenheit= int(input("Please enter the temperature in degrees Fahrenheit: "))
+
+#Calculating Celsius by using the input of Fahrenheit and the Formula below
+Celsius=(Fahrenheit-32) * 5/9
+
+#Printing the result of Celsius
+print("The temperature in degrees Celsius is",Celsius)
