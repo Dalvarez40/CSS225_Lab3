57f73b53935a0b710f72bd3eb09f9b40b970fc32
Dalvarez40 <146905363+Dalvarez40@users.noreply.github.com>
2023-10-16 15:48:01 -0500

    Add files via upload

---
+++ Problem 5.py
@@ -0,0 +1,18 @@
+#Daniel Alvarez
+#10/16/23
+
+#Problem 5 Lab 3
+
+#This program computes the MPG of Car
+
+#Askig user to input the total number of miles driven
+miles_driven= int(input("How many miles were driven?"))
+
+#Asking the user to input the total number of gallons used
+gallons_used= int(input("How many gallons were used?"))
+
+#Calculating the MPG by dividing miles driven and gallons used
+MPG= miles_driven / gallons_used
+
+#Printing the result of MPG
+print("The total MPG is",MPG)
