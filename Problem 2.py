b0759d5ee8f79fbfbafcde03a65199beaa34745f
Dalvarez40 <146905363+Dalvarez40@users.noreply.github.com>
2023-10-16 15:47:08 -0500

    Add files via upload

---
+++ Problem 2.py
@@ -0,0 +1,15 @@
+#Daniel Alvarez
+#10/10/23
+
+#Problem 2 Lab 3
+
+#This program asks the user their name and greets them
+
+#Asking the User their name
+print("What is your name")
+
+#User inputs name
+name=input()
+
+#Prints out Name and Greets the User
+print("Hello there " + name,"nice to meet you!")
