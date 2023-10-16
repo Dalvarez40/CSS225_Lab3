c658b6a7b252294b1f502e4c70d6d7afc90cd7db
Dalvarez40 <146905363+Dalvarez40@users.noreply.github.com>
2023-10-16 15:47:48 -0500

    Add files via upload

---
+++ Problem 4.py
@@ -0,0 +1,15 @@
+#Daniel Alvarez
+#10/15/23
+
+#Problem 4 Lab 3
+
+#This progarm calculates the area of the circle
+
+#Asking User to input the radius of a circle
+radius=int(input("Please enter the radius of a circle: "))
+
+#Calculating the area of circle by mutipliying 3.14 by the radius to the power of 2
+area=3.14* radius**2
+
+#Printing the result of the area
+print("the area of the circle is",area)
